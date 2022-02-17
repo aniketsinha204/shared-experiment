1000e1d3ec9f1f480aff4d7b21e170b6041fc4e9
Aniket Sinha <aniket@Anikets-MacBook-Pro.local>
2022-02-17 16:10:25 +0530

    modified next.config.js for shared folder

--- project-a/next.config.js
+++ project-a/next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
-module.exports = nextConfig;
+const path = require("path");
+
+module.exports = {
+  webpack: (config, { defaultLoaders }) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        "react-native": path.join(
+          __dirname,
+          "node_modules",
+          "react-native-web"
+        ),
+      },
+      modules: [
+        ...config.resolve.modules,
+        path.resolve(__dirname, "node_modules"),
+      ],
+      symlinks: false,
+    };
+
+    config.module.rules.push({
+      test: /\.+(js|jsx|ts|tsx)$/,
+      use: defaultLoaders.babel,
+      include: [path.resolve(__dirname, "..", "common")],
+    });
+
+    return config;
+  },
+};
